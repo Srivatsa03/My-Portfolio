e5203a136e1eb53b4da810b97eaa847691891d23
Srivatsa Kamballa <skamb10@uic.edu>
2026-08-13 17:35:00 -0500

    Remove the screen crawler

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import { data } from "@/data/data";
 import Image from "next/image";
 import { Analytics } from "@vercel/analytics/next";
 import { CommandPalette } from "@/components/command-palette/command-palette";
-import { ScreenCrawler } from "@/components/ui/screen-crawler";
 import { getAllPosts } from "@/lib/blog";
 import { ViewTransitions } from "next-view-transitions";
 
@@ -93,7 +92,6 @@ export default function RootLayout({
               priority
             />
             {children}
-            <ScreenCrawler />
             <Footer />
           </ThemeProvider>
           <Analytics />
